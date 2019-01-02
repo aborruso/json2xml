3e68083bd69a8d36ba5102c304f4bf7a932a368d
aborruso <aborruso@gmail.com>
2019-01-02 21:18:43 +0100

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# JSON2XML
+
+A python tool to convert a JSON source to XML output
+
+# Note
+
+Inspired by this script http://tripsintech.com/xml-to-json-python-script-also-json-to-xml/
---
+++ json2xml.py
@@ -0,0 +1,34 @@
+import json
+import xmltodict
+import re
+
+
+try:   
+   sys.argv[1]
+except IndexError:  
+   print("You must add the name the json input file")
+   sys.exit()
+
+jsonFile = sys.argv[1]
+ 
+with open(jsonFile, 'r') as f:
+    jsonString = f.read()
+f.close()
+
+data = {}
+data['item'] = json.loads(jsonString)
+root = {}
+root['root'] = data
+
+print('JSON input:')
+print(jsonString)
+
+# remove spaces from keys name 
+xmlString = xmltodict.unparse(json.loads((re.sub(r'\s(?=\w+":)',"_",str(json.dumps(root))))), full_document=False,pretty=True)
+ 
+print('\nXML output(output.xml):')
+print(xmlString)
+ 
+with open('output.xml', 'w') as f:
+    f.write(xmlString)
+f.close()
